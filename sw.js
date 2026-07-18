c94007ef37e719b1df327601f67d3343696a8f32
kjsh1028 <kjsh1028@gmail.com>
2026-07-18 11:40:15 +0900

    chore: bump SW to v1.2.0

--- sw.js
+++ sw.js
@@ -1,18 +1,5 @@
-const CACHE = 'wealthtrack-v1.1.0';
+const CACHE = 'wealthtrack-v1.2.0';
 const ASSETS = ['/', '/wealthtrack/', '/wealthtrack/index.html', '/wealthtrack/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-    if (res && res.status === 200 && res.type === 'basic') {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-    }
-    return res;
-  })));
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { if (res && res.status === 200 && res.type === 'basic') { caches.open(CACHE).then(c => c.put(e.request, res.clone())); } return res; }))); });
